172995c39d08653cf5563a3da35ed459203f9816
grafvonbarnez <adb5757@gmail.com>
2013-10-24 13:33:51 -0400

    Added hacked together, probably non-functional backend code
    YOLO I DON'T KNOW JS

--- linkta.pe/linktape.js
+++ linkta.pe/linktape.js
@@ -1,13 +1,95 @@
 var restify = require('restify');
+var MongoClient = require('mongodb').MongoClient;
+var format = require('util').format;
 
-function respond(req, res, next) {
-	res.send('hello ' + req.params.name);
+var host = process.env['MONGO_NODE_DRIVER_HOST'] != null ? process.env['MONGO_NODE_DRIVER_HOST'] : 'localhost';
+var port = process.env['MONGO_NODE_DRIVER_PORT'] != null ? process.env['MONGO_NODE_DRIVER_PORT'] : 27017;
+var serverport = 8080; //Change to whatever, I don't know what the internet is
+
+function getPlaylist(req, res, next) {
+
+	if(!isPlaylist(req.params.pl)) {
+		res.send({});
+	} else {
+		MongoClient.connect(format("mongodb://%s:%s/linktape", host, port), function(err,db) {
+			if(err) { return console.dir(err); }
+
+			var col = db.collection('playlist');
+			col.findOne({"id" : req.params.pl}, function(err, item) {
+				if(err) { return console.dir(err); }
+				res.send( item.data );
+			});
+
+		});	
+	}
+}
+
+function addPlaylist(req, res, next) {
+	var slug = genSlug();
+	
+	while(isPlaylist(slug)) {
+		slug = genSlug();
+	}
+	
+	var pIn = req.params.data;
+	var pl = new Object();
+	pl.id = slug;
+	pl.name = pIn.name;
+	pl.playlist = new Array();
+
+	for(var i = 0; i < pIn.playlist.length; i++) {
+		pl.playlist[i].title = pIn.playlist[i].title;
+		pl.playlist[i].artist = pIn.playlist[i].artist;
+		pl.playlist[i].uri = pIn.playlist[i].uri;
+		pl.playlist[i].song_type = pIn.playlist[i].song_type;
+		pl.playlist[i].video_id = pIn.playlist[i].video_id;
+	}
+
+	MongoClient.connect(format("mongodb://%s:%s/linktape", host, port), function(err,db) {
+		if(err) { return console.dir(err); }
+
+		var col = db.collection('playlist');
+		col.insert(playlist);
+	});
+}
+
+function unShorten(req, res, next) {
+	//Nothing yet
+}
+
+//I don't really get how values pass back from inline functions so you can probably clean this up
+function isPlaylist( slug ) {
+
+	var isIt;
+
+	MongoClient.connect(format("mongodb://%s:%s/linktape", host, port), function(err,db) {
+		if(err) { return console.dir(err); }
+
+		var col = db.collection('playlist');
+		col.findOne( { "id" : slug.toString() }, function (err, item) {
+			if( item == null ) { isIt = false; }
+			else { isIt = true; }
+		});
+
+	});
+	
+	return isIt;
+}
+
+function genSlug() {
+	var hrTime = process.hrtime();
+	var st = (hrTime[0] * 1e9 + hrTime[1]).toString(36);
+	st = st.substring(st.length - 9, st.length);
+	
+	return st;
 }
 
 var server = restify.createServer();
-server.get('/hello/:name', respond);
-server.head('/hello/:name', respond);
+server.get('/p/:pl', getPlaylist);
+server.get('/s/:url', unShorten);
+server.post('/p/:data', addPlaylist);
 
-server.listen(8080, function() {
-	console.log('%s listening at %s', server.name, server.url);
+
+server.listen(serverport, function() {
+  console.log('%s listening at %s', server.name, server.url);
 });
